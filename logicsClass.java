633aec5959093fe2666f9c11962aae9ffdef20c0
chefInCode <jeevan17603@gmail.com>
2026-05-04 09:27:49 +0530

    startWord

--- logicsClass.java
+++ logicsClass.java
@@ -429,4 +429,11 @@ public class logicsClass implements logicsInterface {
         return resStr;
     }
 
+    @Override
+    public String startWord(String str, String word) {
+        if (str.length() >= word.length() && str.substring(1, word.length()).equals(word.substring(1))) {
+            return str.substring(0, word.length());
+        }
+        return "";
+    }
 }
